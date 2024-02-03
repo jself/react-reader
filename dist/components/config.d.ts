b34f05ba3cae2828b4f01fdb9bf2c8d72b259cae
Jeremy Self <jeremy.self@fiveq.com>
2024-02-02 23:44:19 -0600

    Add dist to deploy, don't build prepare

---
+++ dist/components/config.d.ts
@@ -0,0 +1,2 @@
+export declare const DEMO_URL = "/files/9781493445189.epub";
+export declare const DEMO_NAME = "Something";
---
+++ dist/examples/Persist.d.ts
@@ -0,0 +1,1 @@
+export declare const Persist: () => import("react/jsx-runtime").JSX.Element;
